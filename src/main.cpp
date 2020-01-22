332f006f7f48dbc743f47622f860adb82c825ddc
Zachariah Watt <zjwatt@laptop.ames.watt>
2020-01-22 09:12:57 -0600

    Documenting changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+/// @file
+
 #include <string.h>
 #include <iostream>
 
@@ -7,19 +9,30 @@
 #include "DiamondSquare.hpp"
 #include "CubeWorld.hpp"
 
+/** \brief Stores all information parsed from the command line
+ * 
+*/
 struct options
 {
-    bool failure;
-    const char* failureMsg;
-    bool readInput;
-    const char *inputFileName;
-    size_t mapSize;
-    bool writeOutput;
-    const char *outputFileName;
-    bool benchmark;
-    size_t benchmarkIntervals;
+    bool failure; ///< Indicates whether the command line arguments were successfully parsed or not
+    const char* failureMsg; ///< Describes the nature of the error encountered, if any, in parsing the command line arguments
+    bool readInput; ///< True if the user specified an input file
+    const char *inputFileName; ///< The name of the input file
+    size_t mapSize; ///< Size of the map. Defaults to 5
+    bool writeOutput; ///< True if the user specified an output file
+    const char *outputFileName; ///< Name of the output file
+    bool benchmark; ///< True if the user asked for benchmarks
+    size_t benchmarkIntervals; ///< The number of benchmark intervals the user asked for
+    bool help; ///< True if the user asked for the command line options
 };
 
+/**
+ * Gets the name of the input file
+ * @param argIndex The index of the next token after the command switch
+ * @param argc The total number of tokens pulled from the command line
+ * @param argv The raw tokens pulled from the command line
+ * @param opts Stores the information parsed in this function
+*/ 
 int argInputFile(int argIndex, int argc, char *argv[], struct options *opts) {
     opts->readInput = true;
     
@@ -33,6 +46,9 @@ int argInputFile(int argIndex, int argc, char *argv[], struct options *opts) {
     return argIndex + 1;
 }
 
+/** \brief Gets the map size that the user specified
+ * 
+*/ 
 int argMapSize(int argIndex, int argc, char *argv[], struct options *opts) {
     if (argIndex < argc) {
         char *endptr;
@@ -49,6 +65,9 @@ int argMapSize(int argIndex, int argc, char *argv[], struct options *opts) {
     return argIndex + 1;
 }
 
+/** \brief Gets the name of the user-specified output file
+ * 
+*/
 int argOutputFile(int argIndex, int argc, char *argv[], struct options *opts) {
     opts->writeOutput = true;
     
@@ -61,6 +80,9 @@ int argOutputFile(int argIndex, int argc, char *argv[], struct options *opts) {
     return argIndex + 1;
 }
 
+/** \brief Gets the number of benchmark intervals the user specified
+ * 
+*/
 int argBenchmark(int argIndex, int argc, char *argv[], struct options *opts) {
     opts->benchmark = true;
 
@@ -80,6 +102,9 @@ int argBenchmark(int argIndex, int argc, char *argv[], struct options *opts) {
     return argIndex + 1;
 }
 
+/** \brief Get the map size as the power of 2 that the user specified
+ * 
+*/
 int argPower(int argIndex, int argc, char *argv[], struct options *opts) {
     if (argIndex < argc) {
         char *endptr;
@@ -96,8 +121,23 @@ int argPower(int argIndex, int argc, char *argv[], struct options *opts) {
     return argIndex + 1;
 }
 
+/** \brief Indicates that the user has requested the help options
+ * 
+*/ 
+int argHelp(int argIndex, int argc, char *argv[], struct options *opts) {
+    opts->help = true;
+    return argIndex;
+}
+
+/** \brief Parses options from the command line
+ * Finds command switches and calls the appropriate parser methods to handle them
+ * @param argc The number of tokens from the command line
+ * @param argv The raw tokens from the command line
+*/
 struct options parseArguments(int argc, char *argv[]) {
     int argIndex = 1;
+
+    //sets the defaults
     struct options opts = {
         .failure =              false,
         .readInput =            false,
@@ -106,11 +146,18 @@ struct options parseArguments(int argc, char *argv[]) {
         .writeOutput =          false,
         .outputFileName =       NULL,
         .benchmark =            false,
-        .benchmarkIntervals =   0
+        .benchmarkIntervals =   0,
+        .help =                 false
     };
+
+    // Runs through the command line tokens, finds the command switches, and
+    // calls the appropriate methods to parse user specifications, if applicable.
     while (argIndex < argc) {
-        if (!strncmp(argv[argIndex], "-i", 3))
+        if (!strncmp(argv[argIndex], "-i", 3)) // The ! is necessary because 0 is false in a conditional
         {
+            // The output from all of these functions is the index of the next switch, after the user
+            // specifications, if any. The point is to set the index so that the loop is ready to parse
+            // the next swtich.
             argIndex = argInputFile(argIndex + 1, argc, argv, &opts);
         }
         else if (!strncmp(argv[argIndex], "-s", 3))
@@ -127,6 +174,10 @@ struct options parseArguments(int argc, char *argv[]) {
         {
             argIndex = argBenchmark(argIndex + 1, argc, argv, &opts);
         }
+        else if (!strncmp(argv[argIndex], "-h", 2))
+        {
+            argIndex = argHelp(argIndex + 1, argc, argv, &opts);
+        }
         else if (!strncmp(argv[argIndex], "--power", 7))
         {
             argIndex = argPower(argIndex + 1, argc, argv, &opts);
@@ -135,6 +186,9 @@ struct options parseArguments(int argc, char *argv[]) {
         {
             opts.failure = true;
         }
+
+        // If any failure occurs in parsing, the argIndex will be set such that the loop will
+        // immediately exit.
         if (opts.failure) {
             argIndex = argc;
         }
@@ -142,20 +196,31 @@ struct options parseArguments(int argc, char *argv[]) {
     return opts;
 }
 
+/** \brief When benchmarks are run, this function will print the mean and standard deviations of the interval times
+ * @param timings An array of the duration of each interval
+ * @param numTimings The number of elements in timings
+*/
 void printStats(double timings[], size_t numTimings) {
     std::cout << "Mean:\t" << gsl_stats_mean(timings, 1, numTimings) << std::endl;
     std::cout << "Standard Deviation:\t" << gsl_stats_sd(timings, 1, numTimings) << std::endl;
 }
 
+/** \brief Each line of the usage information for this program
+ * 
+*/
 const char *help[] = {
     "Usage:",
     "\tparallel-world [-i <input_file>] [-s <map_size> | -p <map_power>] [-o <output_file>]",
-    "\t\t[-b|--benchmark <intervals>]",
+    "\t\t[-b|--benchmark <intervals>] [ -h ]",
     "\t",
     "\tMap size is the length of the side of a map. It must be 2^n + 1",
     "\t\twhere n is any positive integer."
 };
-const int helpLines = 5;
+const int helpLines = 5; ///< Number of lines in the usage
+
+/*** \brief Print the usage information
+ * 
+*/
 void printHelp() {
     for (size_t i = 0; i < helpLines; i++) {
         std::cout << help[i] << std::endl;
@@ -165,7 +230,7 @@ void printHelp() {
 int main(int argc, char *argv[]) {
     struct options opts = parseArguments(argc, argv);
 
-    if (opts.failure) {
+    if (opts.failure || opts.help) {
         if (opts.failureMsg != NULL) {
             std::cout << opts.failureMsg << "\n" << std::endl;
         }
@@ -180,18 +245,23 @@ int main(int argc, char *argv[]) {
     CubeWorld map(opts.mapSize);
 
     if (opts.benchmark) {
+        // The time durations of the serial intervals and parallel intervals
         double   serialTimes[opts.benchmarkIntervals];
         double parallelTimes[opts.benchmarkIntervals];
 
+        // Run the serial intervals
         for (size_t i = 0; i < opts.benchmarkIntervals; i++) {
             double t1 = omp_get_wtime();
+
             diamondSquare(map);
+
             std::cout
                 << "Generation Time: "
                 << (serialTimes[i] = omp_get_wtime() - t1)
                 << "s"
                 << std::endl
             ;
+            serialTimes[i] = omp_get_wtime() - t1;
         }
         std::cout << "\nSERIAL TIMINGS" << std::endl;
         printStats(serialTimes, opts.benchmarkIntervals);
@@ -199,17 +269,21 @@ int main(int argc, char *argv[]) {
 
         for (size_t i = 0; i < opts.benchmarkIntervals; i++) {
             double t1 = omp_get_wtime();
+
             diamondSquareParallel(map);
+            
             std::cout
                 << "Generation Time: "
                 << (parallelTimes[i] = omp_get_wtime() - t1)
                 << "s"
                 << std::endl
             ;
+            serialTimes[i] = omp_get_wtime() - t1;
         }
         std::cout << "\nPARALLEL TIMINGS" << std::endl;
         printStats(parallelTimes, opts.benchmarkIntervals);
         std::cout << std::endl;
+
     } else {
         diamondSquareParallel(map);
     }
@@ -218,7 +292,6 @@ int main(int argc, char *argv[]) {
     {
         printTerrain(opts.outputFileName, map);
     }
-    
-    
+
     return 0;
 }
